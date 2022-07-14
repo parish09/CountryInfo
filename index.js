81c8955623fc29a9e90fb9cc40c573d061ecd58b
Parishrut Kushwaha <parishrutkushwaha@gmail.com>
2022-07-14 08:31:31 +0530

    Added homepage(ejs file) and added post method that gives us the name of the country user gave as input

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express=require("express");
+const bodyParser=require('body-parser');
 const path=require("path");
 const app=express();
-
+// var country;
+var urlencodedParser=bodyParser.urlencoded({extended: true});
+// app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
-
-
-
-
-
-
-
-
-
-
-
 app.get('/',(req,res)=>{
-    res.send("Hello");
+    res.render('home.ejs');
 })
+app.post('/',urlencodedParser,(req,res)=>{
+    const country=(req.body.country);
+    res.render('home.ejs');
+})
+// console.log(country);
+
+
+
+
+
+
 app.listen(3000,()=>{
     console.log("Server Running Successfully!");
 })
